2decfeb95c1931eb823e62837ae941a9a94cda5a
Greg-2023 <grzegorz.tomasz.badura@gmail.com>
2023-02-22 20:48:30 +0100

    day 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "F";
+var age = 33;
+
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+    Console.WriteLine();
+    Console.WriteLine();
+}
+else if (sex == "F" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (sex == "M" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
