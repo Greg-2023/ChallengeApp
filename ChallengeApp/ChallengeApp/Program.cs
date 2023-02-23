e61d2fd87ce00e11311d5d124f03ce4b878f242e
Greg-2023 <grzegorz.tomasz.badura@gmail.com>
2023-02-23 23:08:01 +0100

    first Class implemented

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,19 +1,38 @@
-﻿string name = "Ewa";
-string sex = "F";
-var age = 33;
+﻿//string name = "Ewa";
+//string sex = "F";
+//var age = 33;
 
 
-if (name == "Ewa" && age == 33)
+//if (name == "Ewa" && age == 33)
+//{
+//    Console.WriteLine("Ewa, lat 33");
+//    Console.WriteLine();
+//    Console.WriteLine();
+//}
+//else if (sex == "F" && age < 30)
+//{
+//    Console.WriteLine("Kobieta poniżej 30 lat");
+//}
+//else if (sex == "M" && age < 18)
+//{
+//    Console.WriteLine("Niepełnoletni Mężczyzna");
+//}
+
+
+
+User user1 = new User("Adam");
+User user2 = new User("Monika");
+User user3 = new User("Zuzia");
+User user4 = new User("Damian");
+
+class User
 {
-    Console.WriteLine("Ewa, lat 33");
-    Console.WriteLine();
-    Console.WriteLine();
-}
-else if (sex == "F" && age < 30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (sex == "M" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
+    private string login;
+    private string password;
+    private string name;
+
+    public User(string login)
+    {
+        this.login = login;
+    }
 }
