b368d55d8287c4f681cf5738650a5640599ec076
Greg-2023 <grzegorz.tomasz.badura@gmail.com>
2023-02-25 16:27:54 +0100

    minimum value

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,29 +1,29 @@
 ﻿int number01 = 60;
-int number02 = 70;
+int number02 = 17;
 int number03 = 90;
 int number04 = 85;
 int number05 = 25;
 
 Console.WriteLine("Entered numbers: " + number01 + " " + number02 + " " + number03 + " " + number04 + " " + number05);
 
-if (number01 > number02)
+if (number01 < number02)
 {
 }
 else number01 = number02;
 
-if (number01 > number03)
+if (number01 < number03)
 {
 }
 else number01 = number03;
 
-if (number01 > number04)
+if (number01 < number04)
 {
 }
 else number01 = number04;
 
-if (number01 > number05)
+if (number01 < number05)
 {
 }
 else number01 = number05;
 
-Console.WriteLine("Maximal number is " + number01);
+Console.WriteLine("Minimum value is " + number01);
