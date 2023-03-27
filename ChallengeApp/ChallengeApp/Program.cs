af2520a456b5c62f83741166f742c36018960ead
Greg-2023 <grzegorz.tomasz.badura@gmail.com>
2023-03-27 15:24:58 +0200

    stworzenie pustej aplikacji

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,8 +1,1 @@
-﻿var number01 = 2;
-var number02 = 5;
-var number03 = 3;
-var number04 = 3;
-var number05 = 5;
-var total = number01 + number02 + number03 + number04 + number05;
-float average = total / 5;
-Console.WriteLine("Average is " + average);
+﻿
