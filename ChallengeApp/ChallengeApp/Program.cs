ed5561dd0a99ab7c1b84c980b0b68489bc7d872c
Greg-2023 <grzegorz.tomasz.badura@gmail.com>
2023-03-29 14:58:47 +0200

    tablica - dni tygodnia

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+int[] grades = new int[5];
+string[] dayOfWeek = new string[7];
+dayOfWeek[0] = "Poniedziałek";
+dayOfWeek[1] = "Wtorek";
+dayOfWeek[2] = "Środa";
+dayOfWeek[3] = "Czwartek";
+dayOfWeek[4] = "Piątek";
+dayOfWeek[5] = "Sobota";
+dayOfWeek[6] = "Niedziela";
+
+for (int i = 0; i < dayOfWeek.Length; i++)
+{
+    Console.WriteLine(dayOfWeek[i]);
+}
